302e84c425379db4254865890624523cc657cf6a
William Rodriguez <wisrovi.rodriguez@gmail.com>
2019-03-20 14:49:10 -0500

    Add files via upload

---
+++ SAMANTHA/API_google/DialogFlow.py
@@ -0,0 +1,13 @@
+#this is a NLP, Natural Language Processing
+
+
+#conceptos a utilizar en cualquier ChatBot
+#Intents: intención de lo que el usuario ha querido decir
+            #se sugieres categorizarlas en basicas y especificas
+#Entities: entradas de usuario, palabras, categorias, frases que determinan la respuesta del bot,
+            # estas van asociadas a los Intents
+#Dialogs: es la estructura de la conversación, es la respuesta del bot a un dato del usuario
+
+#token cliente: 8d75aae159d944d3a8b8b2f20d12b86f
+#desarrollador token: cb1bdeb76f674d34add8246e108a48e2
+
